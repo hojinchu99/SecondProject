921bffb9c2a2291cec3b0100f2c4a8c6294ef912
hojin chu <hojinchu99@gmail.com>
2015-09-22 17:21:26 +0900

    added out "Bitkeeper"

--- Hello.java
+++ Hello.java
@@ -1,8 +1,8 @@
-public class Hello{
+﻿public class Hello{
 	public static void main(String[] args){
 		System.out.println("Hello Git!");
 		System.out.println("What is Git?");
-
+		System.out.println("BitKeeper");
 	}
 }
 
